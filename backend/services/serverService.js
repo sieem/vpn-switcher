f41c874ff205e4ce9ff99bbced90f2549ace20ac
sieem <siem.linx@gmail.com>
2020-03-29 18:15:28 +0200

    create server

--- backend/services/serverService.js
+++ backend/services/serverService.js
@@ -1,3 +1,11 @@
-exports.generateToken = () => {
-    return null
+const axios = require('axios')
+const headers = { 'API-Key': process.env.VULTR_API_KEY }
+
+exports.getSshkeyList = async () => {
+    try {
+        const { data } = await axios.get('https://api.vultr.com/v1/sshkey/list', {headers})
+        return Object.values(data)[0];
+    } catch (error) {
+        throw error
+    }
 }
