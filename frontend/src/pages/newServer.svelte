d67817f6a85ef07a83c387b3c08a108081ea7bc2
Siem Lasseel <slasseel@selligent.com>
2020-03-29 14:42:07 +0200

    first POST call

--- frontend/src/pages/newServer.svelte
+++ frontend/src/pages/newServer.svelte
@@ -1,8 +1,25 @@
 <script>
+    import { ApiService } from '../services/api.service';
+    const api = new ApiService();
+    let activerServers = [];
+    let promise;
 
+    function handleClick() {
+        promise = api.startServer({test:"test"});
+    }
 </script>
 
-newServer
+<button on:click={handleClick}>
+	Generate new server
+</button>
+
+{#await promise}
+	<p>...waiting</p>
+{:then number}
+	<p>The number is {number}</p>
+{:catch error}
+	<p style="color: red">{error.message}</p>
+{/await}
 
 <style>
 
