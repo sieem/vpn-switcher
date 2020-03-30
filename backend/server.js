403f3725b5e9b7d74121b6176716b384cf640dd8
sieem <siem.linx@gmail.com>
2020-03-30 13:04:05 +0200

    use express as static server

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const https = require('https')
 const fs = require('fs')
 const cookieParser = require('cookie-parser')
 const routes = require('./routes')
+const staticRoot = '../frontend/public/'
 
 const app = express()
 
@@ -15,6 +16,25 @@ app.use(cors())
 app.use(cookieParser())
 
 app.use('/api', routes);
+
+app.use(function (req, res, next) {
+    //if the request is not html then move along
+    var accept = req.accepts('html', 'json', 'xml');
+    if (accept !== 'html') {
+        return next();
+    }
+
+    // if the request has a '.' assume that it's for a file, move along
+    var ext = path.extname(req.path);
+    if (ext !== '') {
+        return next();
+    }
+
+    fs.createReadStream(staticRoot + 'index.html').pipe(res);
+
+});
+
+app.use(express.static(staticRoot, { dotfiles: 'allow' }));
 
 //starting up the server
 if (process.env.NODE_ENV == "production") {
