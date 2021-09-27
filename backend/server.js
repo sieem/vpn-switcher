c3ffdabeae1178f519cb76bb6736a4f9f2431348
sieem <siem.linx@gmail.com>
2021-09-27 22:28:13 +0200

    Remove ssl server, it's handled by nginx now

--- backend/server.js
+++ backend/server.js
@@ -37,41 +37,10 @@ app.use(function (req, res, next) {
 app.use(express.static(staticRoot, { dotfiles: 'allow' }));
 
 //starting up the server
-if (process.env.NODE_ENV == "production") {
-    // Certificate
-    const privateKey = fs.readFileSync('/etc/letsencrypt/live/vpnswitcher.siemlasseel.be/privkey.pem', 'utf8');
-    const certificate = fs.readFileSync('/etc/letsencrypt/live/vpnswitcher.siemlasseel.be/cert.pem', 'utf8');
-    const ca = fs.readFileSync('/etc/letsencrypt/live/vpnswitcher.siemlasseel.be/chain.pem', 'utf8');
-
-    const credentials = {
-        key: privateKey,
-        cert: certificate,
-        ca: ca
-    };
-
-    const httpsServer = https.createServer(credentials, app);
-    httpsServer.listen(process.env.PORT, err => {
-        if (err) {
-            return console.error(err);
-        }
-        console.log(`App listening on port ${httpsServer.address().port} SSL`);
-    });
-
-    // setting redirect for non http traffic
-    http.createServer(function (req, res) {
-        res.writeHead(301, { Location: `https://${req.headers.host}${req.url}` });
-        res.end();
-    }).listen(80);
-}
-else if (process.env.NODE_ENV == "development") {
-    const httpServer = http.createServer(app);
-    httpServer.listen(process.env.PORT || 3000, err => {
-        if (err) {
-            return console.error(err);
-        }
-        console.log(`App listening on port ${httpServer.address().port} Non-SSL`);
-    });
-}
-else {
-    throw new Error("No NODE_ENV defined, perhaps run npm start of npm run dev?");
-}
+const httpServer = http.createServer(app);
+httpServer.listen(process.env.PORT || 3000, err => {
+    if (err) {
+        return console.error(err);
+    }
+    console.log(`App listening on port ${httpServer.address().port} Non-SSL`);
+});
